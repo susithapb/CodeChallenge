80e5a88144ab765bc65aee815b48404da12f37fb
Susitha Bandara <susithapb@gmail.com>
2021-08-15 10:12:56 +0800

    Leet Code2

---
+++ LeetCode2/LeetCode2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestFromRight
+{
+    class LeetCode2
+    {
+        static void nextGreatest(int[] arr)
+        {
+            int size = arr.Length;
+
+            int max_from_right = arr[size - 1];
+
+            arr[size - 1] = -1;
+
+            for (int i = size - 2; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = max_from_right;
+                if (temp > max_from_right)
+                    max_from_right = temp;
+
+            }
+
+            foreach (var x in arr)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.ReadLine();
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 16, 17, 4, 3, 5, 2 };
+            nextGreatest(arr);
+        }
+    }
+}
